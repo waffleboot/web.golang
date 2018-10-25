5fc3eca358f0173a01344b868de2e825a105f45a
Andrei Yangabishev <waffleboot@gmail.com>
2018-10-25 04:25:12 +0300

    use net.http file server

--- web.go
+++ web.go
@@ -3,27 +3,17 @@ package main
 
 import (
 	"log"
-	"fmt"
 	"flag"
-	"strings"
 	"path/filepath"
-	"html/template"
 	"os"
-	"sort"	
 	"net/http"
 	"strconv"
 )
 
 var showDir bool
 var files_pattern string
-var dir_html_template *template.Template
 
-type dir_template_context struct {
-	Name string
-	Files []os.FileInfo
-}
-
-func (c dir_template_context) ShowFile(fi os.FileInfo) bool {
+func showFile(fi os.FileInfo) bool {
 	if fi.Name()[0] == 46 {
 		return false
 	} else if fi.IsDir() {
@@ -36,57 +26,9 @@ func (c dir_template_context) ShowFile(fi os.FileInfo) bool {
 	return r
 }
 
-func show_dir(dir string, path string, resp http.ResponseWriter, req *http.Request) {
-	file,e := os.Open(dir)
-	if e != nil {
-		http.NotFound(resp, req)
-		return
-	}
-	defer file.Close()
-	files,e := file.Readdir(0)
-	if e != nil {
-		http.Error(resp, fmt.Sprintf("unable to read content of dir %s", dir),http.StatusInternalServerError)
-		return
-	}
-	sort.Slice(files,func(i,j int)bool{
-		return birthTime(files[i]).After(birthTime(files[j]))
-	})	
-	if dir_html_template.Execute(resp,dir_template_context{path,files}) != nil {
-		http.Error(resp, fmt.Sprintf("unable to show content of dir %s", dir),http.StatusInternalServerError)
-		return
-	}
-}
-
-func load_file(name string, resp http.ResponseWriter, req *http.Request) {
-	log.Printf("load file %s",name)
-	http.ServeFile(resp,req,name)
-}
-
-func make_dir_template() {
-	var e error
-	m := make(map[string]interface{})
-	r := strings.NewReplacer("_"," ","-"," ")
-	m["replace"] = func(s string) string {
-		return r.Replace(s)
-	}
-	dir_html_template,e = template.New("dir").Funcs(m).Parse(`<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 3.2 Final//EN"><html>
-	<title>Directory listing for {{.Name}}</title>
-	<body>
-	<h2>Directory listing for {{.Name}}</h2>
-	<hr>
-	<ul>
-	{{$context := .}}
-	{{range .Files}}
-	{{if $context.ShowFile . }}<li><a href="{{.Name}}">{{ replace .Name }}</a>{{end}}
-	{{end}}
-	</ul>
-	<hr>
-	</body>
-	</html>`)
-	if e != nil {
-		log.Fatalf("unable to parse dir template")
-	}
-}
+func (d webDir) Sort(a os.FileInfo, b os.FileInfo) bool {
+	return birthTime(a).After(birthTime(b))
+} 
 
 func main() {
 
@@ -96,23 +38,5 @@ func main() {
 	flag.Parse()
 
 	cwd,_ := os.Getwd()
-	make_dir_template()
-	http.HandleFunc("/",func (resp http.ResponseWriter, req *http.Request) {
-		name := filepath.Join(cwd,filepath.FromSlash(req.URL.Path))
-		fi,e := os.Stat(name)
-		if e != nil {
-			http.NotFound(resp, req)
-			return
-		}
-		if fi.IsDir() {
-			if strings.HasSuffix(req.URL.Path,"/") {
-				show_dir(name,req.URL.Path,resp,req)
-			} else {
-				http.Redirect(resp,req,req.URL.Path+"/",http.StatusMovedPermanently)
-			}
-		} else {
-			load_file(name,resp,req)
-		}
-	})
-	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(*portPtr),nil))
+	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(*portPtr),http.FileServer(webDir(cwd))))
 }
