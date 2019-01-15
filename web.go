b84983b254547ab5eed7dcc52452a3a5ebd091d8
Andrei Yangabishev <waffleboot@gmail.com>
2019-01-15 16:21:42 +0300

    refactor configuration reader and construct files pattern by config name in absence of config file of line

--- web.go
+++ web.go
@@ -13,12 +13,17 @@ import (
 	"strings"
 )
 
-var showDir bool
-var sortByName bool
-var filesPattern string
+type config struct {
+	port         int
+	showDir      bool
+	sortByName   bool
+	filesPattern string
+}
+
+var conf config = config{8000, false, false, ""}
 
 func (d webDir) Sort(a os.FileInfo, b os.FileInfo) (c bool) {
-	if c = birthTime(a).After(birthTime(b)); sortByName {
+	if c = birthTime(a).After(birthTime(b)); conf.sortByName {
 		c = a.Name() < b.Name()
 	}
 	return
@@ -28,11 +33,11 @@ func (d webDir) Filter(fi os.FileInfo) bool {
 	if fi.Name()[0] == 46 {
 		return false
 	} else if fi.IsDir() {
-		return showDir
-	} else if len(filesPattern) == 0 {
+		return conf.showDir
+	} else if len(conf.filesPattern) == 0 {
 		return true
 	}
-	r, e := filepath.Match(filesPattern, fi.Name())
+	r, e := filepath.Match(conf.filesPattern, fi.Name())
 	if e != nil {
 		return false
 	}
@@ -47,7 +52,7 @@ func readConfig(name string) ([]string, error) {
 	p := filepath.Join(u.HomeDir, ".web.golang")
 	f, err := os.Open(p)
 	if err != nil {
-		return nil, errors.New("config file " + p + " not found")
+		return []string{"-files", "*." + name}, nil
 	}
 	r := bufio.NewReader(f)
 	s := bufio.NewScanner(r)
@@ -61,40 +66,36 @@ func readConfig(name string) ([]string, error) {
 			}
 		}
 	}
-	if err := s.Err(); err != nil {
-		return nil, errors.New("error on scanning " + p + " file")
-	}
-	return nil, errors.New("no entry for " + name)
+	return []string{"-files", "*." + name}, nil
 }
 
-func readArgs(args []string, d1 int, d2 bool, d3 bool, d4 string) (int, bool, bool, string, string) {
+func readArgs(args []string, c config) (config, string) {
 	f := flag.NewFlagSet(os.Args[0], flag.ExitOnError)
-	port := f.Int("port", d1, "port number")
-	showDir := f.Bool("dir", d2, "show dir")
-	sortByName := f.Bool("sort", d3, "sort by name")
-	filesPattern := f.String("files", d4, "files pattern")
+	port := f.Int("port", c.port, "port number")
+	showDir := f.Bool("dir", c.showDir, "show dir")
+	sortByName := f.Bool("sort", c.sortByName, "sort by name")
+	filesPattern := f.String("files", c.filesPattern, "files pattern")
 	f.Parse(args)
 	params := ""
 	if f.NArg() > 0 {
 		params = f.Arg(0)
 	}
-	return *port, *showDir, *sortByName, *filesPattern, params
+	return config{*port, *showDir, *sortByName, *filesPattern}, params
 }
 
 func main() {
 
-	var port int
 	var configName string
-	port, showDir, sortByName, filesPattern, configName = readArgs(os.Args[1:], 8000, false, false, "")
+	conf, configName = readArgs(os.Args[1:], conf)
 	if len(configName) > 0 {
 		configLine, err := readConfig(configName)
 		if err != nil {
 			log.Fatal(err)
 		}
-		port, showDir, sortByName, filesPattern, _ = readArgs(configLine, 8000, false, false, "")
-		port, showDir, sortByName, filesPattern, _ = readArgs(os.Args[1:], port, showDir, sortByName, filesPattern)
+		conf, _ = readArgs(configLine, conf)
+		conf, _ = readArgs(os.Args[1:], conf)
 	}
 
 	cwd, _ := os.Getwd()
-	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(port), http.FileServer(webDir(cwd))))
+	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(conf.port), http.FileServer(webDir(cwd))))
 }
