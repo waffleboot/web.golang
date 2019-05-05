b8b801b05ee3365e71221b7f33fab6a49ea89873
Andrei Yangabishev <waffleboot@gmail.com>
2019-05-06 01:50:58 +0300

    use full names for variables

--- web.go
+++ web.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"bufio"
-	"errors"
 	"flag"
 	"log"
+	"net"
 	"net/http"
+	_ "net/http/pprof"
 	"os"
 	"os/user"
 	"path/filepath"
@@ -13,17 +14,15 @@ import (
 	"strings"
 )
 
-type config struct {
-	port         int
-	showDir      bool
-	sortByName   bool
-	filesPattern string
-}
-
-var conf = config{8000, false, false, ""}
+var (
+	g_port         = flag.Int("port", 8000, "port number")
+	g_showDir      = flag.Bool("dir", false, "show dir")
+	g_sortByName   = flag.Bool("sort", false, "sort by name")
+	g_filesPattern = flag.String("files", "", "files pattern")
+)
 
 func (d webDir) Sort(a os.FileInfo, b os.FileInfo) (c bool) {
-	if c = birthTime(a).After(birthTime(b)); conf.sortByName {
+	if c = birthTime(a).After(birthTime(b)); *g_sortByName {
 		c = a.Name() < b.Name()
 	}
 	return
@@ -33,70 +32,51 @@ func (d webDir) Filter(fi os.FileInfo) bool {
 	if fi.Name()[0] == 46 {
 		return false
 	} else if fi.IsDir() {
-		return conf.showDir
-	} else if len(conf.filesPattern) == 0 {
+		return *g_showDir
+	} else if len(*g_filesPattern) == 0 {
 		return true
 	}
-	r, e := filepath.Match(conf.filesPattern, fi.Name())
+	r, e := filepath.Match(*g_filesPattern, fi.Name())
 	if e != nil {
 		return false
 	}
 	return r
 }
 
-func readConfig(name string) ([]string, error) {
-	u, err := user.Current()
-	if err != nil {
-		return nil, errors.New("no current user")
+func openConfigFile() *os.File {
+	if currentUser, err := user.Current(); err == nil {
+		file, _ := os.Open(filepath.Join(currentUser.HomeDir, ".web.golang"))
+		return file
 	}
-	p := filepath.Join(u.HomeDir, ".web.golang")
-	f, err := os.Open(p)
-	if err != nil {
-		return []string{"-files", "*." + name}, nil
-	}
-	defer f.Close()
-	r := bufio.NewReader(f)
-	s := bufio.NewScanner(r)
-	for s.Scan() {
-		t := strings.TrimSpace(s.Text())
-		f := strings.Fields(t)
-		if len(f) > 1 {
-			k := f[0]
-			if k == name {
-				return f[1:], nil
+	return nil
+}
+
+func readConfig(configKey string) []string {
+	if configFile := openConfigFile(); configFile != nil {
+		defer configFile.Close()
+		r := bufio.NewReader(configFile)
+		s := bufio.NewScanner(r)
+		for s.Scan() {
+			line := strings.TrimSpace(s.Text())
+			fields := strings.Fields(line)
+			if len(fields) > 1 {
+				key := fields[0]
+				if key == configKey {
+					return fields[1:]
+				}
 			}
 		}
 	}
-	return []string{"-files", "*." + name}, nil
-}
-
-func readArgs(args []string) string {
-	f := flag.NewFlagSet(os.Args[0], flag.ExitOnError)
-	f.IntVar(&conf.port, "port", conf.port, "port number")
-	f.BoolVar(&conf.showDir, "dir", conf.showDir, "show dir")
-	f.BoolVar(&conf.sortByName, "sort", conf.sortByName, "sort by name")
-	f.StringVar(&conf.filesPattern, "files", conf.filesPattern, "files pattern")
-	f.Parse(args)
-	params := ""
-	if f.NArg() > 0 {
-		params = f.Arg(0)
-	}
-	return params
+	return []string{"-files", "*." + configKey}
 }
 
 func main() {
-
-	var configName string
-	configName = readArgs(os.Args[1:])
-	if len(configName) > 0 {
-		configLine, err := readConfig(configName)
-		if err != nil {
-			log.Fatal(err)
-		}
-		readArgs(configLine)
-		readArgs(os.Args[1:])
+	flag.Parse()
+	if flag.NArg() > 0 {
+		flag.CommandLine.Parse(readConfig(flag.Arg(0)))
+		flag.CommandLine.Parse(os.Args[1:])
 	}
-
 	cwd, _ := os.Getwd()
-	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(conf.port), http.FileServer(webDir(cwd))))
+	http.Handle("/", http.FileServer(webDir(cwd)))
+	log.Fatal(http.ListenAndServe(net.JoinHostPort("localhost", strconv.Itoa(*g_port)), nil))
 }
